762a66e19adf0375c4c95fbffef78d7fac3ce0b4
gss-18ggh <75316467+gss-18ggh@users.noreply.github.com>
2020-12-17 18:14:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var car, wall;
+var speed, weight;
+
+
+function setup() {
+
+  createCanvas(800,400);
+  car= createSprite(100, 200, 50, 50);
+  speed=random(55,90);
+  //car.velocityX=speed;
+  wall=createSprite(700, 200, 60, height/2)
+  
+  weight=random(400, 1500);
+}
+
+function draw() {
+  background("black");
+  
+  if (keyDown("space")){
+    car.velocityX=speed;
+  }
+    
+  
+  
+  if(wall.x-car.x<(car.width+wall.width)/2){
+car.velocityX=0
+//car.x=100;
+//car.y=200;
+var deformation=0.5*weight*speed*speed/22509;
+if (deformation>180){
+  car.shapeColor=color(255,0,0);
+}
+if(deformation<180 && deformation>100){
+  car.shapeColor=color(230, 230, 0);
+}
+if(deformation<100){
+  car.shapeColor=color(0, 255, 0)
+}
+
+  }
+  
+
+
+  drawSprites();
+
+}
